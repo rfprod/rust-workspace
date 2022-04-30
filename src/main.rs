30571add2da8e46ed23d68b056d83efb0050d4fc
Vadim <zoidenmacht@zoho.com>
2022-05-01 00:01:50 +0300

    feat(sample): add the sample guessting game implementation (#7)
    - [x] add the guessing game tutorial implementation;
    - [x] update recommended vscode extensions and recommended settings;

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use rand::{thread_rng, Rng};
+use std::{cmp::Ordering, io};
+
 fn main() {
-    println!("Hello, world!!!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut range = thread_rng();
+
+    let secret_number: i32 = range.gen_range(1..2);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        let mut guess_input = String::new();
+
+        io::stdin()
+            .read_line(&mut guess_input)
+            .expect("Failed to read line");
+
+        let guess = match guess_input.trim().parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess_input);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
