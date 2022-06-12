05090e665c178b080ac7d7a2300278e5ed976064
Vadim <zraw.stdio@gmail.com>
2022-06-12 16:16:51 +0300

    refactor(oop): refactor the calculator and the guessing game (#10)
    - [x] refactor programs using oop: calculator, guessing game;

--- src/calculator.rs
+++ src/calculator.rs
@@ -5,56 +5,78 @@ use std::{
     io,
 };
 
-// The calculator program entry point.
+// The program entry point.
 pub fn main() {
-    let mut args: Args = args();
-
-    println!("\n{}:\n{:?}", "Arguments".cyan(), args);
-
-    println!("\n{}", "Calculator.".blue());
-
-    let expression_arg = args.nth(2);
-    calculate(expression_arg);
+    let mut calculator = Calculator::new();
+    calculator.init();
 }
 
-// The calculator program expresion parsing and evaluation logic.
-fn calculate(expression_arg: Option<String>) {
-    let mut expression_arg_input = if let Some(..) = expression_arg {
-        match expression_arg.unwrap().trim().parse::<String>() {
-            Ok(value) => value,
-            Err(_) => String::new(),
+struct Calculator;
+
+// The calculator implementation.
+impl Calculator {
+    // Creates a new calculator.
+    fn new() -> Calculator {
+        Calculator
+    }
+
+    // Initializes the calculator.
+    fn init(&mut self) {
+        println!("\n{}", "Calculator initialized.".blue());
+
+        let expression_arg = self.get_expression_arg();
+
+        self.calculate(expression_arg);
+    }
+
+    // Parses the user expression.
+    fn get_expression_arg(&mut self) -> Option<String> {
+        let mut args: Args = args();
+
+        println!("\n{}:\n{:?}", "Arguments".cyan(), args);
+
+        args.nth(2)
+    }
+
+    // The calculator program expresion parsing and evaluation logic.
+    fn calculate(&mut self, expression_arg: Option<String>) {
+        let mut expression_arg_input = if let Some(..) = expression_arg {
+            match expression_arg.unwrap().trim().parse::<String>() {
+                Ok(value) => value,
+                Err(_) => String::new(),
+            }
+        } else {
+            String::new()
+        };
+
+        loop {
+            let mut expression_input = String::new();
+
+            if expression_arg_input.trim().is_empty() && expression_input.trim().is_empty() {
+                println!(
+                    "\n{}",
+                    "Please input an expression without spaces:".yellow()
+                );
+
+                io::stdin()
+                    .read_line(&mut expression_input)
+                    .expect("Failed to read line");
+            } else if expression_input.trim().is_empty() {
+                expression_input = expression_arg_input.to_string();
+            }
+
+            if expression_input.trim().is_empty() {
+                expression_arg_input = String::new();
+                continue;
+            }
+
+            let expression = expression_input.as_str().trim().to_string();
+
+            println!("\n{}: {}", "Your expression".cyan(), expression);
+
+            let result = eval_str(expression.as_str()).unwrap();
+
+            println!("\n{}: {}", "Result".green(), result);
         }
-    } else {
-        String::new()
-    };
-
-    loop {
-        let mut expression_input = String::new();
-
-        if expression_arg_input.trim().is_empty() && expression_input.trim().is_empty() {
-            println!(
-                "\n{}",
-                "Please input an expression without spaces:".yellow()
-            );
-
-            io::stdin()
-                .read_line(&mut expression_input)
-                .expect("Failed to read line");
-        } else if expression_input.trim().is_empty() {
-            expression_input = expression_arg_input.to_string();
-        }
-
-        if expression_input.trim().is_empty() {
-            expression_arg_input = String::new();
-            continue;
-        }
-
-        let expression = expression_input.as_str().trim().to_string();
-
-        println!("\n{}: {}", "Your expression".cyan(), expression);
-
-        let result = eval_str(expression.as_str()).unwrap();
-
-        println!("\n{}: {}", "Result".green(), result);
     }
 }
