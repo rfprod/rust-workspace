bfe6a0cbb168cc710b1d99680e67c53d2c33e651
Vadim <zraw.stdio@gmail.com>
2022-06-24 00:07:09 +0300

    refactor(oop): use oop in all subprograms, bump dependencies (#13)
    refactor(oop): use oop in all subprograms, bump dependencies
    
    - [x] refactor all existing programs, use oop;
    - [x] bump project dependencies;

--- src/system_information.rs
+++ src/system_information.rs
@@ -10,162 +10,180 @@ type Subprograms<'a> = [&'a str; 6];
 
 // The system information program entry point.
 pub fn main() {
-    let mut args: Args = args();
-
-    println!("\n{}:\n{:?}", "Arguments".cyan(), args);
-
-    println!(
-        "\n{}",
-        "Print information about the operating system.".blue()
-    );
-
-    let subprogram_arg = args.nth(2);
-
-    let subprogram_index = choose_subprogram(subprogram_arg);
-
-    let mut system = sysinfo::System::new_all();
-    system.refresh_all();
-
-    match subprogram_index {
-        0 => print_system_information(system),
-        1 => print_processes(system),
-        2 => print_components_temperature(system),
-        3 => print_disks_info(system),
-        4 => print_memory_information(system),
-        5 => print_all_information(system),
-        _ => print_all_information(system),
-    };
+    SystemInformation::new();
 }
 
-// Prompts input from the user, processes it, and returns the selected subprogram index.
-fn choose_subprogram(subprogram_arg: Option<String>) -> usize {
-    let mut subprogram_arg_input = if let Some(..) = subprogram_arg {
-        match subprogram_arg.unwrap().trim().parse::<i32>() {
-            Ok(value) => value.to_string(),
-            Err(_) => String::new(),
-        }
-    } else {
-        String::new()
-    };
+struct SystemInformation;
 
-    let subprograms: Subprograms = [
-        "System information",
-        "Process information",
-        "Components temperature",
-        "Disks information",
-        "Memory information",
-        "All information",
-    ];
+impl SystemInformation {
+    // Creates a new system information instance.
+    fn new() -> SystemInformation {
+        let mut program = SystemInformation;
+        program.init();
+        program
+    }
 
-    loop {
-        let mut subprogram_input = String::new();
+    // Initializes the system information program.
+    fn init(&mut self) {
+        println!("\n{}", "System information initialized.".blue());
 
-        if subprogram_arg_input.is_empty() {
-            print_instructions(subprograms);
+        let subprogram_arg = self.get_subprogram_arg();
 
-            io::stdin()
-                .read_line(&mut subprogram_input)
-                .expect("Failed to read line");
+        let subprogram_index = self.choose_subprogram(subprogram_arg);
+
+        let mut system = sysinfo::System::new_all();
+        system.refresh_all();
+
+        match subprogram_index {
+            0 => self.print_system_information(system),
+            1 => self.print_processes(system),
+            2 => self.print_components_temperature(system),
+            3 => self.print_disks_info(system),
+            4 => self.print_memory_information(system),
+            5 => self.print_all_information(system),
+            _ => self.print_all_information(system),
+        };
+    }
+
+    // Parses the input arguments.
+    fn get_subprogram_arg(&mut self) -> Option<String> {
+        let mut args: Args = args();
+
+        println!("\n{}:\n{:?}", "Arguments".cyan(), args);
+
+        args.nth(2)
+    }
+
+    // Prompts input from the user, processes it, and returns the selected subprogram index.
+    fn choose_subprogram(&mut self, subprogram_arg: Option<String>) -> usize {
+        let mut subprogram_arg_input = if let Some(..) = subprogram_arg {
+            match subprogram_arg.unwrap().trim().parse::<i32>() {
+                Ok(value) => value.to_string(),
+                Err(_) => String::new(),
+            }
         } else {
-            subprogram_input = subprogram_arg_input.to_string();
-        }
-
-        let subprogram_index = match subprogram_input.trim().parse::<usize>() {
-            Ok(num) => num,
-            Err(_) => continue,
+            String::new()
         };
 
-        match subprogram_index.cmp(&subprograms.len()) {
-            Ordering::Less => {
-                return select_subprogram(subprograms, subprogram_index);
+        let subprograms: Subprograms = [
+            "System information",
+            "Process information",
+            "Components temperature",
+            "Disks information",
+            "Memory information",
+            "All information",
+        ];
+
+        loop {
+            let mut subprogram_input = String::new();
+
+            if subprogram_arg_input.is_empty() {
+                self.print_instructions(subprograms);
+
+                io::stdin()
+                    .read_line(&mut subprogram_input)
+                    .expect("Failed to read line");
+            } else {
+                subprogram_input = subprogram_arg_input.to_string();
             }
-            Ordering::Greater => subprogram_arg_input = reset_input_arg(),
-            Ordering::Equal => subprogram_arg_input = reset_input_arg(),
+
+            let subprogram_index = match subprogram_input.trim().parse::<usize>() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+            match subprogram_index.cmp(&subprograms.len()) {
+                Ordering::Less => {
+                    return self.select_subprogram(subprograms, subprogram_index);
+                }
+                Ordering::Greater => subprogram_arg_input = self.reset_input_arg(),
+                Ordering::Equal => subprogram_arg_input = self.reset_input_arg(),
+            }
         }
     }
-}
 
-// Prints the subprogram selection instructions.
-fn print_instructions(subprograms: Subprograms) {
-    println!("\n{}", "Available subprograms:".cyan());
+    // Prints the subprogram selection instructions.
+    fn print_instructions(&mut self, subprograms: Subprograms) {
+        println!("\n{}", "Available subprograms:".cyan());
 
-    let max_i = subprograms.len() - 1;
-    let mut i = 0;
-    while i <= max_i {
-        println!("{}: {}", i, subprograms[i]);
-        i += 1;
+        let max_i = subprograms.len() - 1;
+        let mut i = 0;
+        while i <= max_i {
+            println!("{}: {}", i, subprograms[i]);
+            i += 1;
+        }
+
+        println!("\n{}, [0-{}]:", "Please select a subprogram".cyan(), max_i);
     }
 
-    println!("\n{}, [0-{}]:", "Please select a subprogram".cyan(), max_i);
-}
-
-// Resets the input argument to start over if the program does not exist.
-fn reset_input_arg() -> String {
-    println!("\n{}", "The subprogram does not exist.".red());
-    String::new()
-}
-
-// Prints selected subprogram and returns the program index.
-fn select_subprogram(subprograms: Subprograms, subprogram_index: usize) -> usize {
-    let subprogram = subprograms[subprogram_index];
-    println!("You selected: {}", subprogram);
-    subprogram_index
-}
-
-// Print information about the system.
-fn print_system_information(system: System) -> System {
-    println!("\n{}", "System information:".green());
-    println!("System name:           {:?}", system.get_name());
-    println!("System kernel version: {:?}", system.get_kernel_version());
-    println!("System OS version:     {:?}", system.get_os_version());
-    println!("System host name:      {:?}", system.get_host_name());
-    system
-}
-
-// Print the system processes information.
-fn print_processes(system: System) -> System {
-    println!("\n{}", "System processes:".green());
-    for (pid, proc_) in system.get_processes() {
-        println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
+    // Resets the input argument to start over if the program does not exist.
+    fn reset_input_arg(&mut self) -> String {
+        println!("\n{}", "The subprogram does not exist.".red());
+        String::new()
     }
-    system
-}
 
-// Print the temperature of the components.
-fn print_components_temperature(system: System) -> System {
-    println!("\n{}", "Components temperature:".green());
-    for component in system.get_components() {
-        println!("{:?}", component);
+    // Prints selected subprogram and returns the program index.
+    fn select_subprogram(&mut self, subprograms: Subprograms, subprogram_index: usize) -> usize {
+        let subprogram = subprograms[subprogram_index];
+        println!("You selected: {}", subprogram);
+        subprogram_index
     }
-    system
-}
 
-// Print the disks information.
-fn print_disks_info(system: System) -> System {
-    println!("\n{}", "Disks information:".green());
-    for disk in system.get_disks() {
-        println!("{:?}", disk);
+    // Print information about the system.
+    fn print_system_information(&mut self, system: System) -> System {
+        println!("\n{}", "System information:".green());
+        println!("System name:           {:?}", system.get_name());
+        println!("System kernel version: {:?}", system.get_kernel_version());
+        println!("System OS version:     {:?}", system.get_os_version());
+        println!("System host name:      {:?}", system.get_host_name());
+        system
     }
-    system
-}
 
-// Print the RAM and SWAP information.
-fn print_memory_information(system: System) -> System {
-    println!("\n{}", "Memory information:".green());
-    println!("Total memory: {} KB", system.get_total_memory());
-    println!("Used memory : {} KB", system.get_used_memory());
-    println!("Total swap  : {} KB", system.get_total_swap());
-    println!("Used swap   : {} KB", system.get_used_swap());
-    system
-}
+    // Print the system processes information.
+    fn print_processes(&mut self, system: System) -> System {
+        println!("\n{}", "System processes:".green());
+        for (pid, proc_) in system.get_processes() {
+            println!("{}:{} => status: {:?}", pid, proc_.name(), proc_.status());
+        }
+        system
+    }
 
-// Print all information about the system.
-fn print_all_information(system: System) -> System {
-    let mut sys = system;
-    sys = print_system_information(sys);
-    sys = print_processes(sys);
-    sys = print_components_temperature(sys);
-    sys = print_disks_info(sys);
-    sys = print_memory_information(sys);
-    sys
+    // Print the temperature of the components.
+    fn print_components_temperature(&mut self, system: System) -> System {
+        println!("\n{}", "Components temperature:".green());
+        for component in system.get_components() {
+            println!("{:?}", component);
+        }
+        system
+    }
+
+    // Print the disks information.
+    fn print_disks_info(&mut self, system: System) -> System {
+        println!("\n{}", "Disks information:".green());
+        for disk in system.get_disks() {
+            println!("{:?}", disk);
+        }
+        system
+    }
+
+    // Print the RAM and SWAP information.
+    fn print_memory_information(&mut self, system: System) -> System {
+        println!("\n{}", "Memory information:".green());
+        println!("Total memory: {} KB", system.get_total_memory());
+        println!("Used memory : {} KB", system.get_used_memory());
+        println!("Total swap  : {} KB", system.get_total_swap());
+        println!("Used swap   : {} KB", system.get_used_swap());
+        system
+    }
+
+    // Print all information about the system.
+    fn print_all_information(&mut self, system: System) -> System {
+        let mut sys = system;
+        sys = self.print_system_information(sys);
+        sys = self.print_processes(sys);
+        sys = self.print_components_temperature(sys);
+        sys = self.print_disks_info(sys);
+        sys = self.print_memory_information(sys);
+        sys
+    }
 }
